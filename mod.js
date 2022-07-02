8cf2e2a1a02e697b188f48a87fdb047312d8eb87
rafiul islam <MuhammadRafiulIslam9803>
2022-07-03 00:52:30 +0600

    whats-are-going

---
+++ mod.js
@@ -0,0 +1,24 @@
+document.getElementsByTagName("h1")[1].innerHTML="Barabhita is my Home ";
+
+
+document.getElementsByTagName("p")[0].innerHTML="you got that";
+
+
+
+document.querySelector( ".class0 li").innerHTML="first is done"
+
+
+
+
+document.querySelector( ".class1 a").innerHTML="second is done"
+
+
+function myfunction(){
+     var image=document.querySelector("img");
+     image.src="image/Rafiul.jpg";
+}
+function myfunction2(){           
+    var image=document.querySelector("img");
+    image.src="image/akas.jpg";
+}
+       
